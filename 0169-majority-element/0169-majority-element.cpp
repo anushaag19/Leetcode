e6127690d7a5a9055cbc8662ede1baa06cf3785f
Anusha Agrawal <168533416+anushaag19@users.noreply.github.com>
2026-09-23 21:51:33 +0530

    Time: 12 ms (7.6%), Space: 42.1 MB (5.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n =nums.size();
+        int n = nums.size();
         sort(nums.begin(), nums.end());
         int freq = 1;
         int ans = nums[0];
